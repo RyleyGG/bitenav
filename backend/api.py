9be129cee6b9b95762d0982045f7510a2d7b9b8c
RyleyGG <griffithryley2000@gmail.com>
2023-09-21 22:59:48 -0400

    added content to routers

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from routers import test_router
 
 app = FastAPI()
-
+app.include_router(test_router.router, prefix='/test')
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def someFuncName():
+    return {'message': 'Hello World'}
---
+++ backend/routers/test_router.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, APIRouter
+
+router = APIRouter()
+
+@router.get("/")
+async def someFuncName():
+    return {'message': 'Test Func'}
+
+@router.get('/someotherendpoint')
+async def otherFuncName():
+    return {'message': 'Another func'}
