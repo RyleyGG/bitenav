f2b032889bf841386d168ac745e4352d940f90da
RyleyGG <griffithryley2000@gmail.com>
2023-11-09 19:28:54 -0500

    added hard reset script

---
+++ nuke.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd frontend
+echo "Deleting frontend packages. This may take a minute..."
+rm -rf node_modules
+npm install
+cd ../backend
+docker compose down --volumes
+docker compose build
+cd ..
+./setup.sh
