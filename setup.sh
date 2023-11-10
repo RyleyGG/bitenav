61680c0ce338a8912d4b77c175a54de66865bffd
RyleyGG <griffithryley2000@gmail.com>
2023-11-09 19:24:38 -0500

    reformat setup to shell script and update api_start file to LF

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -f .env ]; then
+    echo "Creating docker .env file with default values..."
+
+    postgres_password=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | base64)
+    auth_secret=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | base64)
+
+    echo "postgres_password=$postgres_password" >> .env
+    echo "auth_secret=$auth_secret" >> .env
+    echo "API_URL=http://127.0.0.1:8000" >> .env
+fi
+
+if [ ! -d ./backend/.venv ]; then
+    echo "Creating and initializing python virtual environment..."
+    python -m venv ./backend/.venv
+    echo "Activating the virtual environment..."
+    source ./backend/.venv/bin/activate
+    pip install -r backend/requirements.txt
+fi
+
+if [ -f "./backend/api_start.sh" ]; then
+    chmod u+rwx "./backend/api_start.sh"
+else
+    echo "Could not find api_start.sh file. Your repository may be corrupted, or need to be re-pulled."
+fi
