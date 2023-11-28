a6365225579348ffc99d1eeb276a9c4b025135c9
RyleyGG <griffithryley2000@gmail.com>
2023-11-27 21:43:15 -0500

    start skeleton for macro tracking page

--- frontend/pages/HomePage.tsx
+++ frontend/pages/HomePage.tsx
@@ -1,18 +1,54 @@
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { Button } from '@rneui/base';
 import SearchMeal from '../components/SearchMeal';
 import DisplayMeal from '../components/DisplayMeal';
+import { ProgressChart } from 'react-native-chart-kit';
+
+const { width, height } = Dimensions.get('window');
 
 const HomePage = ({ navigation }: { navigation: any }) => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    const data = {
+        labels: ['Calories', 'Protein', 'Fat', 'Carbohydrates'], // optional
+        data: [0.4, 0.6, 0.8, 0.1]
+      };
 
-        <Text>Search Page</Text>
-        <Button
-            title="Test Meal Search Function"
-            onPress={() => navigation.navigate('Search')}
-        />
-      </View>
+      const chartConfig = {
+        backgroundColor: "#000000",
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        useShadowColorFromDataset: false, // optional
+      };
+
+      return (
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+          <Text style={{fontSize: 32, fontWeight: 'bold', marginVertical: 20, marginLeft: 20 }}>Welcome!</Text>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <View style={{ 
+                borderRadius: 55,
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#000',
+                width: height > width ? width: 0.5 * width,
+                height: height > width ? 0.35 * height: 0.3 * height
+            }}>
+              <ProgressChart
+                data={data}
+                width={height > width ? width: 0.5 * width}
+                height={height > width ? 0.35 * height: 0.3 * height}
+                strokeWidth={16}
+                radius={32}
+                chartConfig={chartConfig}
+                hideLegend={true}
+              />
+            </View>
+          </View>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button
+                title="Search meals"
+                onPress={() => navigation.navigate('Search')}
+            />
+          </View>
+        </View>
     );
 }
 
